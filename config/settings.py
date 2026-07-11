92d7673b57391cf8f02450fe46cf1ebd693e6ed9
iamfannyhenriques <fanny.henriques@hotmail.com>
2026-07-12 00:27:18 +0200

    Move secret key to environment configuration

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xwol@-3*fib2=5qyeee4sr@r*1snu@itgp65^kj8pdj(ab5(ky'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "temporary-development-key",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

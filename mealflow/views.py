c794301f2e4b2f0b5f1c19749d3b764a0de04f75
iamfannyhenriques <fanny.henriques@hotmail.com>
2026-07-11 17:04:00 +0200

    Update home view to display database recipes

--- mealflow/views.py
+++ mealflow/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+from .models import Recipe
+
+
 def home(request):
-    return render(request, 'mealflow/home.html')
+    recipes = Recipe.objects.all()
+
+    context = {
+        "recipes": recipes,
+    }
+
+    return render(request, "mealflow/home.html", context)

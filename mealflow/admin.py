868e623b0799693f27a6178114ee2b5a3239de42
iamfannyhenriques <fanny.henriques@hotmail.com>
2026-07-11 16:23:56 +0200

    Configure recipe models in Django admin

--- mealflow/admin.py
+++ mealflow/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Ingredient, Recipe, SavedRecipe
+
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 3
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "category",
+        "meal_type",
+        "author",
+        "created_at",
+    )
+    list_filter = ("category", "meal_type")
+    search_fields = ("title", "description")
+    inlines = [IngredientInline]
+
+
+admin.site.register(Category)
+admin.site.register(SavedRecipe)

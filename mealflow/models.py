16b8116343c8d6ac726bab36cccd58f83126c3ed
iamfannyhenriques <fanny.henriques@hotmail.com>
2026-07-11 15:51:05 +0200

    Create category model with default image support

--- mealflow/models.py
+++ mealflow/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Represents a food category and its default recipe image.
+    """
+
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    default_image = models.CharField(
+        max_length=200,
+        help_text=(
+            "Static image path, for example: "
+            "mealflow/images/recipes/default-fish.jpg"
+        ),
+    )
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
